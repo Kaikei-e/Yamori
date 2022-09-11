4dc2376d8874882ee84eb6587221369263437cf6
Kaikei-e <e_kaikei@protonmail.com>
2022-09-12 00:04:21 +0900

    Update:  users table SQL was changed

--- mariadb/initSQL/create_db.sql
+++ mariadb/initSQL/create_db.sql
@@ -3,15 +3,16 @@ CREATE DATABASE yamori;
 
 create table yamori.users
 (
-    id             int AUTO_INCREMENT NOT NULL comment 'user id. auto incremented',
-    created_at     DATETIME(6)        NOT NULL DEFAULT CURRENT_TIMESTAMP(6),
-    updated_at     DATETIME(6)        NOT NULL DEFAULT CURRENT_TIMESTAMP(6) ON UPDATE CURRENT_TIMESTAMP(6),
-    deleted_at     DATETIME(6)        NOT NULL comment 'soft delete',
-    email          varchar(255)       NOT NULL comment 'email address',
-    password       varchar(255)       NOT NULL comment 'password',
-    clearance      tinyint            NOT NULL DEFAULT 0 comment 'is admin user',
-    account_status tinyint            NOT NULL DEFAULT 0 comment 'account status',
-    user_name      varchar(255)       NOT NULL comment 'user name',
-    INDEX         idx_email (id, email),
-    PRIMARY KEY   (id)
+    id                int AUTO_INCREMENT NOT NULL comment 'user id. auto incremented',
+    created_at        DATETIME(6)        NOT NULL DEFAULT CURRENT_TIMESTAMP(6),
+    updated_at        DATETIME(6)        NOT NULL DEFAULT CURRENT_TIMESTAMP(6) ON UPDATE CURRENT_TIMESTAMP(6),
+    deleted_at        DATETIME(6)        NOT NULL comment 'soft delete',
+    email             varchar(255)       NOT NULL comment 'email address',
+    pass_hash         varchar(255)       NOT NULL comment 'password',
+    account_status    tinyint            NOT NULL DEFAULT 0 comment 'account status',
+    user_name         varchar(255)       NOT NULL comment 'user name',
+    user_organization int                NOT NULL comment 'user organization',
+    user_role         int                NOT NULL DEFAULT 0 comment 'user role',
+    INDEX idx_email (id, email),
+    PRIMARY KEY (id)
 ) ENGINE = InnoDB comment 'users for managing guest appointment';
