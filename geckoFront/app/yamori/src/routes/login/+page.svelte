344bc8b8e5000b6ac8fc42b2d439086df95092eb
Kaikei-e <e_kaikei@protonmail.com>
2022-09-13 22:14:06 +0900

    Update: init Svelte with SvelteKit

---
+++ geckoFront/app/yamori/src/routes/login/+page.svelte
@@ -0,0 +1,33 @@
+<script>
+  import { session } from '$app/stores'
+  import { goto } from '$app/navigation'
+
+</script>
+
+<svelte:head>
+  <title> Sign in > Yamori </title>
+</svelte:head>
+  
+<div class="auth-page">
+  <div class="container">
+    <div class="column">
+      <div class="header">
+        <h1>Sign in</h1>
+        <p>Need an account? <a href="/register">Sign up</a></p>
+      </div>
+      <form>
+        <fieldset>
+          <fieldset class="form-group">
+            <input type="email" placeholder="Email" />
+          </fieldset>
+          <fieldset class="form-group">
+            <input type="password" placeholder="Password" />
+          </fieldset>
+          <button class="btn btn-lg btn-primary pull-xs-right" type="submit">
+            Sign in
+          </button>
+        </fieldset>
+      </form>
+    </div>
+  </div>
+</div>
