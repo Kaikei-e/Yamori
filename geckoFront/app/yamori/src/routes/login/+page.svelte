171df6ddbb05167811692fbd6cfd93e51650f24f
Kaikei-e <e_kaikei@protonmail.com>
2022-09-13 22:17:11 +0900

    Update: session is no longer needed

--- geckoFront/app/yamori/src/routes/login/+page.svelte
+++ geckoFront/app/yamori/src/routes/login/+page.svelte
@@ -1,33 +1,45 @@
 <script>
-  import { session } from '$app/stores'
-  import { goto } from '$app/navigation'
+	import { goto } from '$app/navigation';
 
+	let email = '';
+	let password = '';
+	let errors = null;
+
+	async function submit(event) {
+		const response = await post(`auth/login`, { email, password });
+
+		// TODO handle network errors
+		errors = response.errors;
+
+		if (response.user) {
+			$session.user = response.user;
+			goto('/');
+		}
+	}
 </script>
 
 <svelte:head>
-  <title> Sign in > Yamori </title>
+	<title>Sign in > Yamori</title>
 </svelte:head>
-  
+
 <div class="auth-page">
-  <div class="container">
-    <div class="column">
-      <div class="header">
-        <h1>Sign in</h1>
-        <p>Need an account? <a href="/register">Sign up</a></p>
-      </div>
-      <form>
-        <fieldset>
-          <fieldset class="form-group">
-            <input type="email" placeholder="Email" />
-          </fieldset>
-          <fieldset class="form-group">
-            <input type="password" placeholder="Password" />
-          </fieldset>
-          <button class="btn btn-lg btn-primary pull-xs-right" type="submit">
-            Sign in
-          </button>
-        </fieldset>
-      </form>
-    </div>
-  </div>
+	<div class="container">
+		<div class="column">
+			<div class="header">
+				<h1>Sign in</h1>
+				<p>Need an account? <a href="/register">Sign up</a></p>
+			</div>
+			<form>
+				<fieldset>
+					<fieldset class="form-group">
+						<input type="email" placeholder="Email" />
+					</fieldset>
+					<fieldset class="form-group">
+						<input type="password" placeholder="Password" />
+					</fieldset>
+					<button class="btn btn-lg btn-primary pull-xs-right" type="submit"> Sign in </button>
+				</fieldset>
+			</form>
+		</div>
+	</div>
 </div>
