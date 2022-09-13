30c55fd543928789c19fb241cf4da94a32535800
Kaikei-e <e_kaikei@protonmail.com>
2022-09-13 23:18:15 +0900

    Update: learning Svelte and SvelteKit

--- geckoFront/app/yamori/src/routes/login/+page.svelte
+++ geckoFront/app/yamori/src/routes/login/+page.svelte
@@ -1,8 +1,14 @@
 <script>
+	import { session } from '$app/stores';
 	import { goto } from '$app/navigation';
+	import { post } from '$lib/utils.js';
+	import ListErrors from '$lib/ListErrors.svelte';
 
 	let email = '';
 	let password = '';
+	/**
+	 * @type {null}
+	 */
 	let errors = null;
 
 	async function submit(event) {
@@ -19,27 +25,42 @@
 </script>
 
 <svelte:head>
-	<title>Sign in > Yamori</title>
+	<title>Sign in >> Yamori </title>
 </svelte:head>
 
 <div class="auth-page">
-	<div class="container">
-		<div class="column">
-			<div class="header">
-				<h1>Sign in</h1>
-				<p>Need an account? <a href="/register">Sign up</a></p>
-			</div>
-			<form>
-				<fieldset>
+	<div class="container page">
+		<div class="row">
+			<div class="col-md-6 offset-md-3 col-xs-12">
+				<h1 class="text-xs-center">Sign In</h1>
+				<p class="text-xs-center">
+					<a href="/register">Need an account?</a>
+				</p>
+
+				<ListErrors {errors} />
+
+				<form on:submit|preventDefault={submit}>
 					<fieldset class="form-group">
-						<input type="email" placeholder="Email" />
+						<input
+							class="form-control form-control-lg"
+							type="email"
+							required
+							placeholder="Email"
+							bind:value={email}
+						/>
 					</fieldset>
 					<fieldset class="form-group">
-						<input type="password" placeholder="Password" />
+						<input
+							class="form-control form-control-lg"
+							type="password"
+							required
+							placeholder="Password"
+							bind:value={password}
+						/>
 					</fieldset>
 					<button class="btn btn-lg btn-primary pull-xs-right" type="submit"> Sign in </button>
-				</fieldset>
-			</form>
+				</form>
+			</div>
 		</div>
 	</div>
 </div>
